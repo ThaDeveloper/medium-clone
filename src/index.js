2c0cccf1395f3f23987f9cc6215d5f5ad5c1486c
Justin Ndwiga <ndwigajustin@gmail.com>
2018-04-18 10:58:58 +0300

    HTTP requests with superagent and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import App from './Components/App';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import React, {Component } from 'react';
-import { createStore } from 'redux';
+import React from 'react';
+import {  applyMiddleware, createStore } from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+import { promiseMiddleware } from './middleware';
 
 const defaultState = {
   appName: 'Clone Media',
@@ -10,12 +12,18 @@ const defaultState = {
 };
 
 const reducer = function(state = defaultState, action) {
+   switch(action.type) {
+      case 'HOME_PAGE_LOADED':
+       return { ...state, articles: action.payload.articles };
+     }
    return state;
  };
- const store = createStore(reducer);
+//  const store = createStore(reducer);
+ const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render((
   <Provider store={store}>
     <App />
   </Provider>
 ), document.getElementById('root'));
+registerServiceWorker();
